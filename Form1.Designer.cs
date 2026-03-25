10e931750e8b1b32928d39e7d91ea3e006c58ce1
tnwn7909-hub <tnwn7909@gmail.com>
2026-03-25 16:15:27 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace SUSI_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtamount = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnFormat = new System.Windows.Forms.Button();
+            this.cbxdrink = new System.Windows.Forms.ComboBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "주문자 이름";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "음료 종류";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "수량";
+            // 
+            // txtname
+            // 
+            this.txtname.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtname.Location = new System.Drawing.Point(115, 9);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(121, 21);
+            this.txtname.TabIndex = 3;
+            // 
+            // txtamount
+            // 
+            this.txtamount.Location = new System.Drawing.Point(115, 104);
+            this.txtamount.Name = "txtamount";
+            this.txtamount.Size = new System.Drawing.Size(121, 21);
+            this.txtamount.TabIndex = 5;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnCalc.Location = new System.Drawing.Point(33, 193);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 6;
+            this.btnCalc.Text = "계산";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnFormat
+            // 
+            this.btnFormat.Location = new System.Drawing.Point(213, 192);
+            this.btnFormat.Name = "btnFormat";
+            this.btnFormat.Size = new System.Drawing.Size(75, 23);
+            this.btnFormat.TabIndex = 7;
+            this.btnFormat.Text = "초기화";
+            this.btnFormat.UseVisualStyleBackColor = true;
+            this.btnFormat.Click += new System.EventHandler(this.btnFormat_Click);
+            // 
+            // cbxdrink
+            // 
+            this.cbxdrink.FormattingEnabled = true;
+            this.cbxdrink.Location = new System.Drawing.Point(115, 60);
+            this.cbxdrink.Name = "cbxdrink";
+            this.cbxdrink.Size = new System.Drawing.Size(121, 20);
+            this.cbxdrink.TabIndex = 9;
+            this.cbxdrink.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(27, 247);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(261, 183);
+            this.txtResult.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(326, 472);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.cbxdrink);
+            this.Controls.Add(this.btnFormat);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.txtamount);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "카페 주문 프로그램";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtamount;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnFormat;
+        private System.Windows.Forms.ComboBox cbxdrink;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace SUSI_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbxdrink.Items.Add("아메리카노");
+            cbxdrink.Items.Add("카페라테");
+            cbxdrink.Items.Add("카푸치노");
+
+            cbxdrink.SelectedItem = "아메리카노";
+        }
+
+
+         private void btnCalc_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            string drink = cbxdrink.SelectedItem.ToString();
+
+            double amount = double.Parse(txtamount.Text);
+
+            double price = 0;
+
+
+            if (drink == "아메리카노")
+                price = 3000;
+            else if (drink == "카페라테")
+                price = 4000;
+            else if (drink == "카푸치노")
+                price = 4500;
+
+            double total = price * amount;
+
+
+            double discount = 0;
+            if (amount >= 3)
+            {
+                discount = total / 10;
+            }
+
+            double finalPrice = total - discount;
+
+            txtResult.Text =
+                "주문자: " + name + "\r\n" +
+                "주문내용: " + drink + " " + amount + "잔\r\n" +
+                "주문금액: " + total + "원\r\n" +
+                "할인금액: " + discount + "원\r\n" +
+                "결제금액: " + finalPrice + "원";
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            
+            txtname.Text = "";
+            cbxdrink.Text = "아메리카노";
+            txtamount.Text = "";
+            txtResult.Text = "";
+
+
+        }
+    }
+}
+
