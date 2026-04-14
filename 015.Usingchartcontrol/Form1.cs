f7001f7230a94b13864a55673ceb70ef6b5c449e
tnwn7909-hub <tnwn7909@gmail.com>
2026-04-14 11:51:21 +0900

    Add files via upload

---
+++ 015.Usingchartcontrol/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.ChartDashStyle.Dash;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _015.Usingchartcontrol
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //x축 설정
+            chart1.ChartAreas[0].AxisX.Minimum = -20;
+            chart1.ChartAreas[0].AxisX.Maximum = 20;
+            chart1.ChartAreas[0].AxisX.Interval = 2;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Gray;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+            //y축 설정
+            chart1.ChartAreas[0].AxisY.Minimum = -2;
+            chart1.ChartAreas[0].AxisY.Maximum = 2;
+            chart1.ChartAreas[0].AxisY.Interval = 0.5;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.Gray;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineDashStyle = ChartDashStyle.Dash;
+
+
+            // series[0] : sin(x) / x
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].Color = Color.LightGreen;
+            chart1.Series[0].BorderWidth = 2;
+            chart1.Series[0].LegendText = "sin(x)/x";
+
+
+            // series[1] : cos(x) / x
+            chart1.Series.Add("Cos");
+            chart1.Series[1].ChartType = SeriesChartType.Line;
+            chart1.Series[1].Color = Color.Orange;
+            chart1.Series[1].BorderWidth = 2;
+            chart1.Series[1].LegendText = "cos(x)/x";
+
+
+            for (double x = -20; x <= 20; x += 0.1)
+            {
+                double y = Math.Sin(x) / x;
+                chart1.Series[0].Points.AddXY(x, y);
+
+                y = Math.Cos(x) / x;
+                chart1.Series[1].Points.AddXY(x, y);
+            }
+
+            chart1.ChartAreas[0].BackColor = Color.DarkBlue;
+
+
+        }
+    }
+}
