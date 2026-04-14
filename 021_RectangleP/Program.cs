9a3819c3b9bb82c00455890790c826c8954ca597
tnwn7909-hub <tnwn7909@gmail.com>
2026-04-14 11:54:40 +0900

    Add files via upload

---
+++ 021_RectangleP/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _021_RectangleP
+{
+
+    class Rectangle
+    {
+        public int width { get; set; } // 속성(대문자) public
+        public int height { get; set; }
+        public int GetArea()
+        {
+            return width * height;
+        }
+        
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.width = 10;
+            r.height = 10;
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ 022_Clock/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace _022_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.표시형식ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.아날로그ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.디지털ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝내기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.stopGoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.표시형식ToolStripMenuItem,
+            this.끝내기ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 25);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 표시형식ToolStripMenuItem
+            // 
+            this.표시형식ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.아날로그ToolStripMenuItem,
+            this.디지털ToolStripMenuItem,
+            this.stopGoToolStripMenuItem});
+            this.표시형식ToolStripMenuItem.Name = "표시형식ToolStripMenuItem";
+            this.표시형식ToolStripMenuItem.Size = new System.Drawing.Size(67, 21);
+            this.표시형식ToolStripMenuItem.Text = "표시형식";
+            // 
+            // 아날로그ToolStripMenuItem
+            // 
+            this.아날로그ToolStripMenuItem.Name = "아날로그ToolStripMenuItem";
+            this.아날로그ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.아날로그ToolStripMenuItem.Text = "아날로그";
+            this.아날로그ToolStripMenuItem.Click += new System.EventHandler(this.아날로그ToolStripMenuItem_Click);
+            // 
+            // 디지털ToolStripMenuItem
+            // 
+            this.디지털ToolStripMenuItem.Name = "디지털ToolStripMenuItem";
+            this.디지털ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.디지털ToolStripMenuItem.Text = "디지털";
+            // 
+            // 끝내기ToolStripMenuItem
+            // 
+            this.끝내기ToolStripMenuItem.Font = new System.Drawing.Font("맑은 고딕", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
+            this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(59, 21);
+            this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblDate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.lblDate.Location = new System.Drawing.Point(68, 100);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(83, 32);
+            this.lblDate.TabIndex = 1;
+            this.lblDate.Text = "label1";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(70, 222);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(38, 12);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "label2";
+            // 
+            // stopGoToolStripMenuItem
+            // 
+            this.stopGoToolStripMenuItem.Name = "stopGoToolStripMenuItem";
+            this.stopGoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.stopGoToolStripMenuItem.Text = "Stop / Go";
+            this.stopGoToolStripMenuItem.Click += new System.EventHandler(this.stopGoToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "디지털 시계";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 표시형식ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 아날로그ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 디지털ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝내기ToolStripMenuItem;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.ToolStripMenuItem stopGoToolStripMenuItem;
+    }
+}
+
